bf6bc265899ff30c9c0735f4ee20765524577d33
aiga-youcan <spammerbot679@gmail.com>
2026-09-10 12:55:12 +0100

    Update project

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,19 +4,33 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+// 💡 Storage w bootstrap f Temp folder
+$storagePath = sys_get_temp_dir() . '/campusfix_storage';
+if (!file_exists($storagePath)) {
+    mkdir($storagePath, 0777, true);
+}
+$app->useStoragePath($storagePath);
+
+$bootstrapPath = sys_get_temp_dir() . '/campusfix_bootstrap';
+if (!file_exists($bootstrapPath . '/cache')) {
+    mkdir($bootstrapPath . '/cache', 0777, true);
+}
+$app->useBootstrapPath($bootstrapPath);
+
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
 );
 
+// 💡 Hna rddinaha t-pointi 3la Laravel Core Console Kernel mashi l-fichier l-maḥalli
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    \Illuminate\Foundation\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    \Illuminate\Foundation\Exceptions\Handler::class
 );
 
 return $app;
